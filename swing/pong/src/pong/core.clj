6a0c4552edac32b55eac7cd81f2e83789eb75a98
Huang Yenshan <yenshan@gk9.so-net.ne.jp>
2016-04-16 17:51:14 +0900

    did refactoring

--- swing/pong/src/pong/core.clj
+++ swing/pong/src/pong/core.clj
@@ -4,22 +4,24 @@
   (:gen-class))
 
 (defn new-ball
-  ([x y w h vx vy] {:x x :y y :w w :h h :vx vx :vy vy}))
+  ([x y w h vx vy] {:x x :y y :w w :h h :vx vx :vy vy})
+  ([bal x y vx vy] (assoc bal :x x :y y :vx vx :vy vy)))
+
 
 (defn new-bar [x y w h] {:x x :y y :w w :h h})
 
 (def ball (atom (new-ball 0 0 10 10 1 1)))
 (def bar (atom (new-bar 200 300 50 10)))
 
-(defn move-left
-  [bar]
-  (reset! bar (assoc @bar
-                     :x (- (:x @bar) 5))))
-(defn move-right
-  [bar]
-  (reset! bar (assoc @bar
-                     :x (+ (:x @bar) 5))))
 
+(defn move-bar
+  [dat dr]
+  (let [d (dr {:left - :right +})
+        x (:x @dat)]
+    (->> (d x 5)
+         (assoc @dat :x)
+         (reset! dat))))
+      
 (defn nextv
   [v pos rng]
   (if (and (<= pos (- rng 10))
@@ -27,15 +29,22 @@
     v
     (- v)))
 
-(defn next-ball
+(defn move-by-velocity
+  [obj]
+  (let [{x :x y :y vx :vx vy :vy} obj]
+    (assoc obj :x (+ x vx) :y (+ y vy))))
+
+(defn change-velocity
+  [obj max-x max-y]
+  (let [{x :x y :y vx :vx vy :vy}  obj]
+    (assoc obj 
+           :vx (nextv vx x max-x)
+           :vy (nextv vy y max-y))))
+
+(defn move-ball
   [bal max-x max-y]
-  (let [vx (:vx bal) 
-        vy (:vy bal)
-        x (+ (:x bal) vx)
-        y (+ (:y bal) vy)]
-        (new-ball x y 
-                  (:w bal) (:h bal)
-                  (nextv vx x max-x) (nextv vy y max-y))))
+  (reset! ball (move-by-velocity @ball))
+  (reset! ball (change-velocity @ball max-x max-y)))
 
 (defn draw-ball
   [bal g]
@@ -59,7 +68,7 @@
     (paintComponent [g]
       (let [w (proxy-super getWidth)
             h (proxy-super getHeight)]
-        (reset! ball (next-ball @ball w h))
+        (move-ball ball w h)
         (draw-ball @ball g)
         (draw-bar @bar g)
         ))
@@ -70,8 +79,8 @@
   (proxy [KeyListener] []
     (keyPressed [e]
       (let [kc (.getKeyCode e)]
-        (cond (= kc 37) (move-left bar) 
-              (= kc 39) (move-right bar))))
+        (cond (= kc 37) (move-bar bar :left)
+              (= kc 39) (move-bar bar :right))))
     (keyReleased [e])
     (keyTyped [e])))
 
