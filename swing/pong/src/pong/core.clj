c4960fc8ad65fe4b41fc76c98e606e60b085a3aa
Huang Yenshan <yenshan@gk9.so-net.ne.jp>
2016-04-16 15:20:35 +0900

    add move-left, move-rigth of bar

--- swing/pong/src/pong/core.clj
+++ swing/pong/src/pong/core.clj
@@ -1,16 +1,24 @@
 (ns pong.core
   (:import [javax.swing JFrame JPanel Timer]
-           [java.awt.event ActionListener])
+           [java.awt.event ActionListener KeyListener])
   (:gen-class))
 
 (defn new-ball
-  ([x y w h vx vy] {:x x :y y :w w :h h :vx vx :vy vy})
-  ([bal x y vx vy] (assoc bal :x x :y y :vx vx :vy vy)))
+  ([x y w h vx vy] {:x x :y y :w w :h h :vx vx :vy vy}))
 
 (defn new-bar [x y w h] {:x x :y y :w w :h h})
 
 (def ball (atom (new-ball 0 0 10 10 1 1)))
 (def bar (atom (new-bar 200 300 50 10)))
+
+(defn move-left
+  [bar]
+  (reset! bar (assoc @bar
+                     :x (- (:x @bar) 5))))
+(defn move-right
+  [bar]
+  (reset! bar (assoc @bar
+                     :x (+ (:x @bar) 5))))
 
 (defn nextv
   [v pos rng]
@@ -21,12 +29,12 @@
 
 (defn next-ball
   [bal max-x max-y]
-  (let [vx (:vx @ball) 
-        vy (:vy @ball)
-        x (+ (:x @ball) vx)
-        y (+ (:y @ball) vy)]
+  (let [vx (:vx bal) 
+        vy (:vy bal)
+        x (+ (:x bal) vx)
+        y (+ (:y bal) vy)]
         (new-ball x y 
-                  (:w @ball) (:h @ball)
+                  (:w bal) (:h bal)
                   (nextv vx x max-x) (nextv vy y max-y))))
 
 (defn draw-ball
@@ -45,7 +53,8 @@
       (.setColor java.awt.Color/BLACK)
       (.fillRect x y w h))))
 
-(def panel 
+(defn main-panel 
+  []
   (proxy [JPanel ActionListener] []
     (paintComponent [g]
       (let [w (proxy-super getWidth)
@@ -57,10 +66,23 @@
     (actionPerformed [e]
       (.repaint this))))
 
+(def klistn
+  (proxy [KeyListener] []
+    (keyPressed [e]
+      (let [kc (.getKeyCode e)]
+        (cond (= kc 37) (move-left bar) 
+              (= kc 39) (move-right bar))))
+    (keyReleased [e])
+    (keyTyped [e])))
+
 (defn -main
   [& args]
-  (let [frame (JFrame. "Test Paint")
+  (let [panel (main-panel) 
+        frame (JFrame. "Test Paint")
         timer (Timer. 10 panel)]
+    (doto panel
+      (.setFocusable true)
+      (.addKeyListener klistn))
     (doto frame
       (.add panel)
       (.pack)
