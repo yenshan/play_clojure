15962e3f997322970098d2e54a9e992c1e5264d9
Huang Yenshan <yenshan@gk9.so-net.ne.jp>
2016-04-29 08:46:14 +0900

    refactoring

--- swing/teetris/src/teetris/core.clj
+++ swing/teetris/src/teetris/core.clj
@@ -3,67 +3,76 @@
            [java.awt.event ActionListener KeyListener KeyEvent])
   (:gen-class))
 
-(def I-type [1 1 1 1
-             0 0 0 0])
-(def O-type [1 1 0 0
-             1 1 0 0])
-(def S-type [0 1 1 0
-             1 1 0 0])
-(def Z-type [1 1 0 0
-             0 1 1 0])
-(def L-type [1 0 0 0
-             1 1 1 0])
-(def J-type [0 0 1 0
-             1 1 1 0])
-(def T-type [1 1 1 0
-             0 1 0 0])
+(def BLOCK-SIZE 20)
+(def FIELD-WIDTH 10)
+(def FIELD-HEIGHT 20)
+
+(def block-type {
+                 :I [1 1 1 1
+                     0 0 0 0]
+                 :O [1 1 0 0
+                     1 1 0 0]
+                 :S [0 1 1 0
+                     1 1 0 0]
+                 :Z [1 1 0 0
+                     0 1 1 0]
+                 :L [1 0 0 0
+                     1 1 1 0]
+                 :J [0 0 1 0
+                     1 1 1 0]
+                 :T [1 1 1 0
+                     0 1 0 0]
+                 })
+
+(def block-table (vals block-type))
+
+(def ppx (atom 5))
+(def ppy (atom 0))
+(def bti (atom 0))
+(def angl (atom 0))
+
 
 (defn draw-box
   [g x y]
   (doto g 
     (.setColor java.awt.Color/BLACK)
-    (.fillRect x y 18 18)))
+    (.fillRect x y 19 19)))
 
 
 (defn cal-h
   [i x op] 
-  (-> i (mod 4) (* 18) (->> (op x))))
+  (-> i (mod 4) (* BLOCK-SIZE) (->> (op x))))
 (defn cal-h-p [i x] (cal-h i x +))
 (defn cal-h-m [i x] (cal-h i x -))
 
 (defn cal-v 
   [i y op]
-  (if (< i 4) y (op y 18)))
+  (if (< i 4) y (op y BLOCK-SIZE)))
 (defn cal-v-p [i y] (cal-v i y +))
 (defn cal-v-m [i y] (cal-v i y -))
 
 (defn draw-block
-  [g x y angl col]
-  (doseq [i (range 0 (count col))]
-    (let [e (nth col i)
-          opx ({0 cal-h-p, 90 cal-v-p, 180 cal-h-m, 270 cal-v-m} angl)
-          opy ({0 cal-v-p, 90 cal-h-m, 180 cal-v-m, 270 cal-h-p} angl)
-          px (opx i x)
-          py (opy i y)]
-      (when (= e 1) (draw-box g px py)))))
+  [g ix iy angl col]
+  (let [x (* ix BLOCK-SIZE)
+        y (* iy BLOCK-SIZE)]
+    (doseq [i (range 0 (count col))]
+      (let [e (nth col i)
+            opx ({0 cal-h-p, 90 cal-v-p, 180 cal-h-m, 270 cal-v-m} angl)
+            opy ({0 cal-v-p, 90 cal-h-m, 180 cal-v-m, 270 cal-h-p} angl)
+            px (opx i x)
+            py (opy i y)]
+        (when (= e 1) (draw-box g px py))))))
 
-(def block-table [I-type O-type S-type Z-type L-type J-type T-type])
-
-(def ppx (atom 100))
-(def ppy (atom 0))
-(def bti (atom 0))
-(def angl (atom 0))
 
 (defn main-panel 
   []
   (proxy [JPanel ActionListener] []
     (paintComponent [g]
-      (let [w (proxy-super getWidth)
-            h (proxy-super getHeight)
-            block-type (nth block-table @bti)]
+      (let [block-type (nth block-table @bti)]
         (draw-block g @ppx @ppy @angl block-type)
-        (swap! ppy + 18)
-        (when (>= @ppy h) 
+        (swap! ppy + 1)
+        (println @ppy)
+        (when (>= @ppy FIELD-HEIGHT) 
           (do (reset! ppy 0)
               (swap! bti #(mod (inc %) 7))))
         ))
@@ -74,8 +83,8 @@
   (proxy [KeyListener] []
     (keyPressed [e]
       (let [kc (.getKeyCode e)]
-        (cond (= kc KeyEvent/VK_LEFT) (swap! ppx - 18)
-              (= kc KeyEvent/VK_RIGHT) (swap! ppx + 18)
+        (cond (= kc KeyEvent/VK_LEFT) (swap! ppx - 1)
+              (= kc KeyEvent/VK_RIGHT) (swap! ppx + 1)
               (= kc KeyEvent/VK_DOWN) (swap! angl #(mod (+ % 90) 360)))))
     (keyReleased [e])
     (keyTyped [e])))
@@ -92,6 +101,6 @@
       (.add panel)
       (.pack)
       (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
-      (.setSize 256 400)
+      (.setSize (* FIELD-WIDTH BLOCK-SIZE) (* FIELD-HEIGHT BLOCK-SIZE))
       (.setVisible true))
     (.start timer)))
