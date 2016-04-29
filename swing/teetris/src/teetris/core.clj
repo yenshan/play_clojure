18db4ac1755e5b8cf3e105ec1a730d41794b42cc
Huang Yenshan <yenshan@gk9.so-net.ne.jp>
2016-04-30 00:23:21 +0900

    add hitting check. refactoring

--- swing/teetris/src/teetris/core.clj
+++ swing/teetris/src/teetris/core.clj
@@ -29,39 +29,58 @@
                   :type :I :angle 0}))
 
 (defn pos->idx [x y] (-> y (* FIELD-WIDTH) (+ x)))
-(defn has-block?
-  ([fld idx] (= 1 (nth fld idx)))
-  ([fld x y] (= 1 (nth fld (pos->idx x y)))))
-
-(defn set-block
-  [fld x y] (assoc fld (pos->idx x y) 1))
 
 (defn next-type
   [t]
   (let [tbl (keys block-type)
-        idx (.indexOf tbl t)]
-    (if (= idx (last tbl))
-      (nth tbl 0) 
-      (nth tbl (inc idx)))))
+        nidx (-> tbl
+                 (.indexOf t)
+                 (inc)
+                 (mod (count tbl)))]
+    (nth tbl nidx)))
 
-(defn draw-box
-  [g x y]
-  (let [px (-> x (* BLOCK-SIZE) (+ 1))
-        py (-> y (* BLOCK-SIZE) (+ 1))
-        sz (- BLOCK-SIZE 2)]
-    (doto g 
-      (.setColor java.awt.Color/BLACK)
-      (.fillRect px py sz sz))))
+;;
+;; pure functions for field
+;;
+(defn has-box?
+  ([fld idx] (= 1 (nth fld idx)))
+  ([fld x y]
+   (if (or (< x 0) (< y 0))
+           false
+           (has-box? fld (pos->idx x y)))))
 
-(defn cal-h
-  [i x op] 
-  (-> i (mod 4) (->> (op x))))
+(defn set-box
+  [fld x y] (assoc fld (pos->idx x y) 1))
+
+(defn set-line
+  [fld line]
+  (loop [i 0 fld_ fld]
+    (if (>= i FIELD-WIDTH)
+      fld_
+      (recur (inc i) (set-box fld_ i line)))))
+
+;;
+;; pure functions for block
+;;
+(defn block-dat-size
+  [{typ :type}]
+  (count (block-type typ)))
+
+(defn block-nth
+  [{typ :type} i]
+  (let [col (block-type typ)]
+    (if (< i (count col))
+      (nth col i)
+      0)))
+
+;;
+;; pure functions for calc block drawing
+;;
+(defn cal-h [i x op] (-> i (mod 4) (->> (op x))))
 (defn cal-h-p [i x] (cal-h i x +))
 (defn cal-h-m [i x] (cal-h i x -))
 
-(defn cal-v 
-  [i y op]
-  (if (< i 4) y (op y 1)))
+(defn cal-v [i y op] (if (< i 4) y (op y 1)))
 (defn cal-v-p [i y] (cal-v i y +))
 (defn cal-v-m [i y] (cal-v i y -))
 
@@ -75,53 +94,82 @@
   (let [opy ({0 cal-v-p, 90 cal-h-m, 180 cal-v-m, 270 cal-h-p} angl)]
     (opy i y)))
 
-(defn draw-block
-  [g blk]
-  (let [{x :x y :y angl :angle typ :type} blk
-        col (block-type typ)]
-    (doseq [i (range 0 (count col))]
-      (let [e (nth col i)
-            px (next-x x i angl)
-            py (next-y y i angl)]
-        (when (= e 1) (draw-box g px py))))))
-
+;;
+;; pure functions for field using block
+;;
 (defn set-block-to-fld
   [fld blk i]
-  (let [{x :x y :y angl :angle typ :type} blk
-        col (block-type typ)
-        e (if (< (count col)) (nth col i) 0)
+  (let [{x :x y :y angl :angle} blk
+        e (block-nth blk i)
         px (next-x x i angl)
         py (next-y y i angl)]
     (if (= e 1) 
-      (set-block fld px py)
+      (set-box fld px py)
       fld)))
 
 (defn put-block
   [fld blk]
     (loop [i 0, fld_ fld]
-      (if (>= i 8)
+      (if (>= i (block-dat-size blk))
           fld_
           (let [fld2 (set-block-to-fld fld_ blk i)]
             (recur (inc i) fld2)))))
+  
+(defn blk-e-hit? 
+  [fld blk i]
+  (let [{x :x y :y angl :angle} blk
+        e (block-nth blk i)
+        px (next-x x i angl)
+        py (next-y y i angl)]
+    (if (= e 1)
+      (has-box? fld px py)
+      false)))
+
+(defn blk-hit?
+  [fld blk]
+  (some #(blk-e-hit? fld blk %) (range (block-dat-size blk))))
+       
+;;
+;; 
+;;
+(defn draw-box
+  [g x y]
+  (let [px (-> x (* BLOCK-SIZE) (+ 1))
+        py (-> y (* BLOCK-SIZE) (+ 1))
+        sz (- BLOCK-SIZE 2)]
+    (doto g 
+      (.setColor java.awt.Color/BLACK)
+      (.fillRect px py sz sz))))
+
+(defn draw-block
+  [g blk]
+  (let [{x :x y :y angl :angle} blk]
+    (doseq [i (range (block-dat-size blk))]
+      (let [e (block-nth blk i)
+            px (next-x x i angl)
+            py (next-y y i angl)]
+        (when (= e 1) (draw-box g px py))))))
 
 (defn draw-field
   [g col]
   (doseq [i (range 0 (count col))]
     (let [x (mod i FIELD-WIDTH)
           y (quot i FIELD-WIDTH)]
-      (when (has-block? col i)
+      (when (has-box? col i)
         (draw-box g x y)))))
 
 (def game-loop 
   (proxy [ActionListener] []
     (actionPerformed [e]
-      (if (< (:y @block) 18)
-        (swap! block update :y inc)
-        (do
-          (swap! field put-block @block)
-          (swap! block assoc :y 0)
-          (swap! block update :type next-type)
-          )))))
+      (let [next-blk (update @block :y inc)]
+        ;(if (< (:y @block) 18)
+        (if-not (blk-hit? @field next-blk)
+          (swap! block update :y inc)
+          (do
+            (swap! field put-block @block)
+            (swap! block assoc :y 0)
+            (swap! block update :type next-type)
+            ))))))
 
 
 (defn rotate-left [angl] (-> angl (+ 90) (mod 360)))
@@ -146,7 +194,7 @@
 
 (defn -main
   [& args]
-  (swap! field set-block 4 9)
+  (swap! field set-line 18)
   (doto main-panel
     (.setFocusable true)
     (.addKeyListener klistn))
