7d6034347c0d7b0c7a364e49164ea4b5bb1b5d72
hifistar <yenshan@gk9.so-net.ne.jp>
2017-06-30 23:32:06 +0900

    add test/exp

---
+++ test/exp/src/exp/core.clj
@@ -0,0 +1,35 @@
+(ns exp.core
+  (:gen-class))
+
+(defn exp [base e]
+  (cond 
+    (zero? e) 1
+    :else (* base (exp base (dec e)))))
+
+(defn iter-exp
+  ([base e] (exp2 base e 1))
+  ([base e res]
+   (cond
+     (zero? e) res
+     :else (recur base (dec e) (* base res)))))
+
+(defn fast-exp
+  ([base e] (exp3 base e 1N))
+  ([base e res]
+   (cond
+     (zero? e) res
+     (even? e) (recur (* base base) (/ e 2) res)
+     :else (recur base (dec e) (* base res)))))
+
+(defn expmod [base e m]
+  (cond
+    (zero? e) 1
+    :else (-> (expmod base (dec e) m)
+              (* base)
+              (mod m))))
+
+(defn -main
+  [& args]
+  (println (exp 3 3))
+  (println (iter-exp 4 4))
+  (println (fast-exp 5 10)))
