54dc8f14fd6460aa4cb599101f5adaaec0d785c8
hifistar <yenshan@gk9.so-net.ne.jp>
2017-07-15 16:41:10 +0900

    add 2.19

---
+++ sicp/exercise/2.19/change-counting/test/change_counting/core_test.clj
@@ -0,0 +1,7 @@
+(ns change-counting.core-test
+  (:require [clojure.test :refer :all]
+            [change-counting.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
