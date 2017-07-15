7aef48a3e15c512047b96e92e7f3752aae7ebd25
hifistar <yenshan@gk9.so-net.ne.jp>
2017-07-15 14:05:39 +0900

    add p-cons

---
+++ sicp/p-cons/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to p-cons
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ sicp/p-cons/test/p_cons/core_test.clj
@@ -0,0 +1,7 @@
+(ns p-cons.core-test
+  (:require [clojure.test :refer :all]
+            [p-cons.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
