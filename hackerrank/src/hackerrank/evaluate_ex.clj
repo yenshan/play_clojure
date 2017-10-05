f39ab9dce60fcf3bc32e5b51e66a62c8e3a3a486
hifistar <yenshan@gk9.so-net.ne.jp>
2017-10-06 07:09:59 +0900

    function name revised

--- hackerrank/src/hackerrank/evaluate_ex.clj
+++ hackerrank/src/hackerrank/evaluate_ex.clj
@@ -8,28 +8,28 @@
 ;; 1 + x + x^2/2! + x^3/3! + x^4/4! ...
 ;; Evaluate for given values of by using the above expansion for the first 10 terms. 
 
-(defn expt [n]
+(defn factorial [n]
   (if (= n 1)
     1
-    (* n (expt (dec n)))))
+    (* n (factorial (dec n)))))
 
-(defn pow [b n]
+(defn exponentation [b n]
   (if (zero? n)
     1
-    (* b (pow b (dec n)))))
+    (* b (exponentation b (dec n)))))
 
-(defn e-power-x [x]
+(defn e-expo-x [x]
   (reduce #(+ %1 
-              (/ (pow x %2) (expt %2)))
+              (/ (exponentation x %2) (factorial %2)))
           1.0
           (range 1 10)))
 
 (deftest a-test
-  (testing "e-power-x"
-    (is (= "2423600.1887" (format "%.4f" (e-power-x 20.0000))))
-    (is (= "143.6895" (format "%.4f" (e-power-x 5.0000))))
-    (is (= "1.6487" (format "%.4f" (e-power-x 0.5000))))
-    (is (= "0.6065" (format "%.4f" (e-power-x -0.5000))))
+  (testing "e-expo-x"
+    (is (= "2423600.1887" (format "%.4f" (e-expo-x 20.0000))))
+    (is (= "143.6895" (format "%.4f" (e-expo-x 5.0000))))
+    (is (= "1.6487" (format "%.4f" (e-expo-x 0.5000))))
+    (is (= "0.6065" (format "%.4f" (e-expo-x -0.5000))))
     ))
 
 
