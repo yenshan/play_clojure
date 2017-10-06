c10af238cbdf0601314c8f8a92921fe6b28fb927
hifistar <yenshan@gk9.so-net.ne.jp>
2017-10-06 20:09:15 +0900

    refactoring

--- hackerrank/src/hackerrank/clj/perimeter_polygon.clj
+++ hackerrank/src/hackerrank/clj/perimeter_polygon.clj
@@ -7,11 +7,9 @@
 
 (defn square [x] (* x x))
 
-(defn len [p1 p2]
-  (let [x1 (first p1)
-        y1 (second p1)
-        x2 (first p2)
-        y2 (second p2)]
+(defn distance [p1 p2]
+  (let [x1 (first p1) y1 (second p1)
+        x2 (first p2) y2 (second p2)]
     (Math/sqrt (+ (square (- x2 x1)) 
                   (square (- y2 y1))))))
 
@@ -21,13 +19,17 @@
    (if (< (count coll) 2)
      res
      (recur (rest coll)
-            (+ res (len (first coll) (second coll)))))))
+            (+ res (distance (first coll) (second coll)))))))
 
 (defn str->pair [string]
   (let [d (str/split string #" ")]
     [(Integer/parseInt (first d)) (Integer/parseInt (second d))]))
 
-(let [nt (Integer/parseInt (read-line))]
-  (let [datas (for [i (range nt)]
-                (str->pair (read-line)))]
-    (println (perimeter (vec datas)))))
+(defn read-pair-numbers [n]
+  (vec (for [i (range n)]
+    (str->pair (read-line)))))
+
+(->> (Integer/parseInt (read-line))
+     read-pair-numbers
+     perimeter
+     println)
