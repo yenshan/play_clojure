3a24bfa4c2d43d957a69c6195edfd59c053eea37
hifistar <yenshan@gk9.so-net.ne.jp>
2017-10-06 23:27:52 +0900

    add files

---
+++ hackerrank/src/hackerrank/clj/area_polygon.clj
@@ -0,0 +1,31 @@
+#!/usr/bin/env clj
+
+(ns hackerrank.clj.area-polygon
+  (:require [clojure.string :as str]))
+
+
+(defn sum-products
+  ([f coll] (sum-products f (conj coll (first coll)) 0))
+  ([f coll res]
+   (if (< (count coll) 2)
+     res
+     (recur f (rest coll) (+ res (f (first coll) (second coll)))))))
+
+(defn area-polygon [coll]
+  (let [sum-l (sum-products #(* (first %1) (second %2)) coll)
+        sum-r (sum-products #(* (first %2) (second %1)) coll)]
+    (/ (Math/abs (- sum-l sum-r)) 2.0)))
+
+
+(defn str->pair [string]
+  (let [d (str/split string #" ")]
+    [(Integer/parseInt (first d)) (Integer/parseInt (second d))]))
+
+(defn read-pair-numbers [n]
+  (vec (for [i (range n)]
+    (str->pair (read-line)))))
+
+(->> (Integer/parseInt (read-line))
+     read-pair-numbers
+     area-polygon
+     println)
