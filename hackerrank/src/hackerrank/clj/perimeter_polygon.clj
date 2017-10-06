96eb346ea07dfbb62213a3ed5bf764cbfc2d3600
hifistar <yenshan@gk9.so-net.ne.jp>
2017-10-06 19:58:31 +0900

    add files

---
+++ hackerrank/src/hackerrank/clj/perimeter_polygon.clj
@@ -0,0 +1,30 @@
+#!/usr/bin/env clj
+(ns hackerrank.clj.perimeter-polygon
+  (:require [clojure.string :as str]))
+
+(defn square [x] (* x x))
+
+(defn len [p1 p2]
+  (let [x1 (first p1)
+        y1 (second p1)
+        x2 (first p2)
+        y2 (second p2)]
+    (Math/sqrt (+ (square (- x2 x1)) 
+                  (square (- y2 y1))))))
+
+(defn perimeter
+  ([coll] (perimeter (conj coll (first coll)) 0))
+  ([coll res]
+   (if (< (count coll) 2)
+     res
+     (recur (rest coll)
+            (+ res (len (first coll) (second coll)))))))
+
+(defn str->pair [string]
+  (let [d (str/split string #" ")]
+    [(Integer/parseInt (first d)) (Integer/parseInt (second d))]))
+
+(let [nt (Integer/parseInt (read-line))]
+  (let [datas (for [i (range nt)]
+                (str->pair (read-line)))]
+    (println (Math/round (perimeter (vec datas))))))
