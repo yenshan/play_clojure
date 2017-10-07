849f47ca889f04fc8eb30baee1fb19b21aa2a3ab
hifistar <yenshan@gk9.so-net.ne.jp>
2017-10-07 17:22:38 +0900

    add file

---
+++ hackerrank/src/hackerrank/clj/gcd.clj
@@ -0,0 +1,9 @@
+;;
+;; https://www.hackerrank.com/challenges/functional-programming-warmups-in-recursion---gcd/problem
+;;
+(let [f (fn [a b] 
+          (cond (= a b) a
+                (> a b) (recur (- a b) b)
+                :else (recur (- b a) a)))
+      [m n] (map read-string (re-seq #"\d+" (read-line)))]
+  (println (f m  n)))
