2d50ecd103e8914c1058e039e8261de1445503b4
hifistar <yenshan@gk9.so-net.ne.jp>
2017-10-07 17:47:48 +0900

    add file

---
+++ hackerrank/src/hackerrank/clj/fib.clj
@@ -0,0 +1,11 @@
+;;
+;; https://www.hackerrank.com/challenges/functional-programming-warmups-in-recursion---fibonacci-numbers/problem
+;;
+(ns hackerrank.clj.fib)
+
+(defn fib [a b]
+  (cons a (lazy-seq (fib b (+ a b)))))
+
+(println (nth (fib 0 1) (Integer/parseInt (read-line))))
+
+
