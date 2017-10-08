0fc4353b614880db227654cc155d1dd4c0edfa51
hifistar <yenshan@gk9.so-net.ne.jp>
2017-10-08 22:18:16 +0900

    add file

---
+++ hackerrank/src/hackerrank/clj/string_mingling.clj
@@ -0,0 +1,12 @@
+(ns hackerrank.clj.string-mingling)
+
+(defn merge-string
+  ([s1 s2] (merge-string s1 s2 []))
+  ([s1 s2 res]
+   (if (empty? s1)
+     (apply str res)
+     (recur (rest s1)
+            (rest s2)
+            (conj res (first s1) (first s2))))))
+
+(println (merge-string (read-line) (read-line)))
