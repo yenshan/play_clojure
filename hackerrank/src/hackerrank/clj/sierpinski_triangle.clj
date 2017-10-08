efe67a42624c056f85ab1ec453f80ceb2a52e0cb
hifistar <yenshan@gk9.so-net.ne.jp>
2017-10-08 15:15:02 +0900

    revised

--- hackerrank/src/hackerrank/clj/sierpinski_triangle.clj
+++ hackerrank/src/hackerrank/clj/sierpinski_triangle.clj
@@ -1,10 +1,5 @@
 (ns hackerrank.clj.sierpinski-triangle)
 
-(defn bottom-len [h]
-  (if (= h 1)
-    1
-    (+ 2 (bottom-len (dec h)))))
-
 (defn next-row [coll]
   (vec 
     (reduce (fn [res [x y]] 
@@ -15,24 +10,33 @@
             #{}
             coll)))
 
-
 (defn triangle
   "座標posを頂点に高さheightの三角形を作成する。
   三角形を構成する点[x y]のlazy sequenceを返す。"
   [pos height]
   (letfn [(iter [p]
             (cons p (lazy-seq (iter (next-row p)))))]
-    (take height (iter [pos]))))
+    (apply concat (take height (iter [pos])))))
 
-(defn divide-triangle
+(defn bottom-len [h]
+  (if (= h 1)
+    1
+    (+ 2 (bottom-len (dec h)))))
+
+(defn fractal-triangle
   [[x y] h]
   (let [new-h (quot h 2)
         new-bottom-len (bottom-len new-h)
         offset (inc (quot new-bottom-len 2))]
-    (reduce concat (concat
-                     (triangle [x y] new-h)
-                     (triangle [(- x offset) (+ y new-h)] new-h)
-                     (triangle [(+ x offset) (+ y new-h)] new-h)))))
+    [(list [x y] new-h)
+     (list [(- x offset) (+ y new-h)] new-h)
+     (list [(+ x offset) (+ y new-h)] new-h)]))
+    
+(defn fractal-triangle-n-time [N tri]
+  (if (zero? N)
+    tri
+    (recur (dec N)
+           (apply concat (map #(fractal-triangle (first %) (second %)) tri)))))
 
 (defn draw-triangle-on-area
   [w h coll]
@@ -43,7 +47,10 @@
         (print \1)
         (print \_))))
 
-(draw-triangle-on-area 63 32 
-                       (divide-triangle [31 0] 32))
-
+(defn draw-sierpinski-triangle [N]
+  (->> (fractal-triangle-n-time N [(list [31 0] 32)])
+       (reduce (fn [res d] 
+                 (concat res (triangle (first d) (second d))))
+               '())
+       (draw-triangle-on-area 63 32)))
 
