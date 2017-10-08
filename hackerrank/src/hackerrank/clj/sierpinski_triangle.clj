9d8bd17066d0ec18af8d1095e10971af9e029def
hifistar <yenshan@gk9.so-net.ne.jp>
2017-10-08 10:22:04 +0900

    revised

--- hackerrank/src/hackerrank/clj/sierpinski_triangle.clj
+++ hackerrank/src/hackerrank/clj/sierpinski_triangle.clj
@@ -1,5 +1,10 @@
 (ns hackerrank.clj.sierpinski-triangle)
 
+(defn bottom-len [h]
+  (if (= h 1)
+    1
+    (+ 2 (bottom-len (dec h)))))
+
 (defn next-row [coll]
   (vec 
     (reduce (fn [res [x y]] 
@@ -10,22 +15,35 @@
             #{}
             coll)))
 
-(defn triangle [pos]
-  (cons pos (lazy-seq (triangle (next-row pos)))))
 
-(defn triangle-pos-list [pos n]
-  (take n (triangle [pos])))
+(defn triangle
+  "座標posを頂点に高さheightの三角形を作成する。
+  三角形を構成する点[x y]のlazy sequenceを返す。"
+  [pos height]
+  (letfn [(iter [p]
+            (cons p (lazy-seq (iter (next-row p)))))]
+    (take height (iter [pos]))))
 
-(defn sierpinski-triangle [w h x y]
-  (let [dot-tri (reduce concat (triangle-pos-list [x y] h))]
-    (for [iy (range h)
-          ix (range w)]
-      (if (some #(= % [ix iy]) dot-tri)
-        \1
-        \_))))
+(defn divide-triangle
+  [[x y] h]
+  (let [new-h (quot h 2)
+        new-bottom-len (bottom-len new-h)
+        offset (inc (quot new-bottom-len 2))]
+    (reduce concat (concat
+                     (triangle [x y] new-h)
+                     (triangle [(- x offset) (+ y new-h)] new-h)
+                     (triangle [(+ x offset) (+ y new-h)] new-h)))))
+
+(defn draw-triangle-on-area
+  [w h coll]
+    (doseq [iy (range h)
+            ix (range w)]
+      (when (zero? ix) (print "\n"))
+      (if (some #(= % [ix iy]) coll)
+        (print \1)
+        (print \_))))
+
+(draw-triangle-on-area 63 32 
+                       (divide-triangle [31 0] 32))
 
 
-(doall (map-indexed (fn [i e]
-               (when (zero? (mod i 63)) (print "\n"))
-               (print e))
-             (sierpinski-triangle 63 32 (quot 63 2) 0)))
