d54d56ae8445eed059ad9ea82ceaa6c5fe31319a
hifistar <yenshan@gk9.so-net.ne.jp>
2017-10-15 21:10:37 +0900

    revised

--- hackerrank/src/hackerrank/clj/crossword_101.clj
+++ hackerrank/src/hackerrank/clj/crossword_101.clj
@@ -1,38 +1,22 @@
 (ns hackerrank.clj.crossword-101)
 
 
-
-(def str-array ["LONDON" "DELHI" "ICELAND" "ANKARA"])
-
-(defn get-empty-pos 
-  ([coll y] (get-empty-pos coll y 0 []))
-  ([coll y x res]
-   (if (empty? coll)
-     res
-     (if (= \- (first coll))
-       (recur (rest coll) y (inc x) (conj res [x y]))
-       (recur (rest coll) y (inc x) res)))))
-
 (defn vec->map [coll]
   {:x (first coll)
    :y (second coll)})
 
-(def dat (map vec->map '([1 0] [1 1] [1 2] [1 3] [2 3] [3 3] [4 3] [5 3] [1 4] [5 4] [1 5] [5 5] [5 6] [2 7] [3 7] [4 7] [5 7] [6 7] [7 7] [5 8] [5 9])))
-
-;(def dat (apply concat (for [i (range 10)]
-;                         (get-empty-pos (read-line) i))))
-
-
-
-(defn sequential-along-axis?
-  ([axis coll] (-sequential? axis (rest coll) (first coll)))
-  ([axis coll pre]
-   (if (empty? coll)
-     true
-     (let [e (first coll)]
-       (if (= (inc (axis pre)) (axis e))
-         (recur axis (rest coll) e)
-         false)))))
+(defn sequential-of-pos?
+  "座標の列collの各要素posが1ずつ増加しているかチェックする。
+  pos .. :x or :y"
+  [pos coll]
+  (letfn [(iter [coll pre]
+            (if (empty? coll)
+              true
+              (let [e (first coll)]
+                (if (= (inc (pos pre)) (pos e))
+                  (recur (rest coll) e)
+                  false))))]
+    (iter (rest coll) (first coll))))
 
 (defn axis-complement [a]
   (condp = a
@@ -50,15 +34,24 @@
            (filter #(= (oaxis e) (oaxis %)) coll))
          set
          (filter #(not= 1 (count %)))
-         (filter #(sequential-along-axis? axis %)))))
-         
-(find-coordinate-along-axis :y dat)
-  
+         (filter #(sequential-of-pos? axis %)))))
 
-(for [e dat]
-  {:dat e
-   :vertical (filter #(= (first e) (first %)) dat)
-   :horizontal (filter #(= (second e) (second %)) dat)})
+;(for [e dat]
+;  {:dat e
+;   :vertical (filter #(= (first e) (first %)) dat)
+;   :horizontal (filter #(= (second e) (second %)) dat)})
 
 
+(def str-array ["LONDON" "DELHI" "ICELAND" "ANKARA"])
 
+(defn get-empty-pos 
+  [y coll]
+  (->> (map (fn [a b] [a b]) (range (count coll)) coll)
+       (filter (fn [[n o]] (= \- o)))
+       (map (fn [[n o]] [n y]))))
+
+(def dat (map vec->map (apply concat (for [i (range 10)]
+                                      (get-empty-pos i (read-line))))))
+
+(println (find-coordinate-along-axis :y dat))
+(println (find-coordinate-along-axis :x dat))
