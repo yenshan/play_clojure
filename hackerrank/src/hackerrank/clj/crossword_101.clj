c0c24757631a8399ed295ad088ea0326844e4883
hifistar <yenshan@gk9.so-net.ne.jp>
2017-10-16 22:37:03 +0900

    add test code

--- hackerrank/src/hackerrank/clj/crossword_101.clj
+++ hackerrank/src/hackerrank/clj/crossword_101.clj
@@ -42,16 +42,56 @@
 ;   :horizontal (filter #(= (second e) (second %)) dat)})
 
 
-(def str-array ["LONDON" "DELHI" "ICELAND" "ANKARA"])
-
 (defn get-empty-pos 
   [y coll]
   (->> (map (fn [a b] [a b]) (range (count coll)) coll)
        (filter (fn [[n o]] (= \- o)))
        (map (fn [[n o]] [n y]))))
 
-(def dat (map vec->map (apply concat (for [i (range 10)]
-                                      (get-empty-pos i (read-line))))))
+;(def dat (map vec->map (apply concat (for [i (range 10)]
+;                                      (get-empty-pos i (read-line))))))
 
-(println (find-coordinate-along-axis :y dat))
-(println (find-coordinate-along-axis :x dat))
+;(println (find-coordinate-along-axis :y dat))
+;(println (find-coordinate-along-axis :x dat))
+
+
+(def test-boxes (list ['A 6] ['B 5] ['C 7] ['D 6])) 
+(def test-strs ["LONDON" "DELHI" "ICELAND" "ANKARA"])
+
+(println boxes)
+
+(defn find-fit-box [len coll]
+  (some (fn [d] (if (= len (second d)) d nil))
+        coll))
+
+(find-fit-box 7 test-boxes)
+
+;(defn all-patterns [strs boxes]
+;  (let [s (first strs)
+;        fb (find-fit-box (count s) boxes)]
+;    (when (not (nil? fb))
+;      (cons [fb s] (all-patterns (rest strs)
+;                                 (remove #(= % fb) boxes))))))
+
+
+(def test-dat1 [1 2 3 4])
+(def test-dat2 [\a \b \c \d])
+
+(defn all-patterns 
+  ([coll1 coll2] (all-patterns coll1 coll2 []))
+  ([coll1 coll2 res]
+   (if (empty? coll1)
+     res
+     (let [r (for [e coll1]
+               (all-patterns (remove #(= % e) coll1) 
+                             (rest coll2)
+                             (conj res [e (first coll2)])))]
+       (if (= (count coll1) 1)
+         r
+         (apply concat r))))))
+
+(all-patterns test-dat1 test-dat2)
+
+
+
+
