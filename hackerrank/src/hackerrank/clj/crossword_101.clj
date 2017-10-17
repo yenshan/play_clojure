ffb1f4ac9cedc98ffc83a90798b7e2a8734b64e4
hifistar <yenshan@gk9.so-net.ne.jp>
2017-10-17 23:14:39 +0900

    add implementation

--- hackerrank/src/hackerrank/clj/crossword_101.clj
+++ hackerrank/src/hackerrank/clj/crossword_101.clj
@@ -36,62 +36,53 @@
          (filter #(not= 1 (count %)))
          (filter #(sequential-of-pos? axis %)))))
 
-;(for [e dat]
-;  {:dat e
-;   :vertical (filter #(= (first e) (first %)) dat)
-;   :horizontal (filter #(= (second e) (second %)) dat)})
-
-
 (defn get-empty-pos 
   [y coll]
   (->> (map (fn [a b] [a b]) (range (count coll)) coll)
        (filter (fn [[n o]] (= \- o)))
        (map (fn [[n o]] [n y]))))
 
-;(def dat (map vec->map (apply concat (for [i (range 10)]
-;                                      (get-empty-pos i (read-line))))))
+(defn get-empty-boxes [dat]
+  (map (fn [dat] {:len (count dat) :dat dat})
+       (apply concat (for [a [:x :y]]
+                       (find-coordinate-along-axis a dat)))))
 
-;(println (find-coordinate-along-axis :y dat))
-;(println (find-coordinate-along-axis :x dat))
 
-
-(def test-boxes (list ['A 6] ['B 5] ['C 7] ['D 6])) 
+(def test-boxes (list {:dat 'A :len 6} {:dat 'B :len 5} {:dat 'C :len 7} {:dat 'D :len 6})) 
 (def test-strs ["LONDON" "DELHI" "ICELAND" "ANKARA"])
 
-(println boxes)
-
-(defn find-fit-box [len coll]
-  (some (fn [d] (if (= len (second d)) d nil))
-        coll))
-
-(find-fit-box 7 test-boxes)
-
-;(defn all-patterns [strs boxes]
-;  (let [s (first strs)
-;        fb (find-fit-box (count s) boxes)]
-;    (when (not (nil? fb))
-;      (cons [fb s] (all-patterns (rest strs)
-;                                 (remove #(= % fb) boxes))))))
-
-
-(def test-dat1 [1 2 3 4])
-(def test-dat2 [\a \b \c \d])
-
-(defn all-patterns 
-  ([coll1 coll2] (all-patterns coll1 coll2 []))
-  ([coll1 coll2 res]
-   (if (empty? coll1)
+(defn all-patterns-fill-boxes
+  ([strs boxes] (all-patterns-fill-boxes strs boxes []))
+  ([strs boxes res]
+   (if (empty? strs)
      res
-     (let [r (for [e coll1]
-               (all-patterns (remove #(= % e) coll1) 
-                             (rest coll2)
-                             (conj res [e (first coll2)])))]
-       (if (= (count coll1) 1)
+     (let [r (for [e strs]
+               (all-patterns-fill-boxes (remove #(= % e) strs) 
+                                        (rest boxes)
+                                        (conj res [e (first boxes)])))]
+       (if (= (count strs) 1)
          r
          (apply concat r))))))
 
-(all-patterns test-dat1 test-dat2)
+(defn patterns-of-put-str-in-box
+  "空きボックスに文字列を当てはめてみた、すべてのパターンを返す。"
+  [strs boxes]
+  (letfn [(has-some-wrong-len? [coll]
+            (some (fn [[string {len :len}]] (not= (count string) len)) coll))]
+    (filter #(not (has-some-wrong-len? %)) (all-patterns-fill-boxes strs boxes))))
 
 
+(defn read-lines->all-empty-positions
+  [lines]
+  (map vec->map
+       (apply concat (for [i (range lines)]
+                       (get-empty-pos i (read-line))))))
+
+;;---
+;;---
+(def boxes (get-empty-boxes (read-line->all-empty-positions 10)))
+
+(doseq [i (patterns-of-put-str-in-box test-strs boxes)]
+  (println i))
 
 
