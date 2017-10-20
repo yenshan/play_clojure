85555168262cf421381dcbc93b3f4c53531a3e6e
hifistar <yenshan@gk9.so-net.ne.jp>
2017-10-20 22:11:43 +0900

    add implemtation patterns

--- hackerrank/src/hackerrank/filter_pos_in_a_list.clj
+++ hackerrank/src/hackerrank/filter_pos_in_a_list.clj
@@ -1,17 +1,26 @@
 (ns hackerrank.filter-pos-in-a-list
   (:require [clojure.test :refer :all]))
 
-(defn delim-odd-pos 
-  ([coll] (delim-odd-pos 1 coll '()))
-  ([i coll res]
-   (if (empty? coll)
-     res
-     (if (even? i)
-       (recur (inc i) (rest coll) (concat res (list (first coll))))
-       (recur (inc i) (rest coll) res)))))
+
+(defn delim-odd-pos [coll]
+  (letfn [(iter [i coll res]
+            (if (empty? coll)
+              res
+              (if (even? i)
+                (recur (inc i) (rest coll) (conj res (first coll)))
+                (recur (inc i) (rest coll) res))))]
+    (iter 1 coll [])))
+
+(defn delim-odd-pos2 [coll]
+  (->> coll
+       (map (fn [a b] [a b]) (range 1 (inc (count coll))))
+       (filter (fn [[a b]] (even? a)))
+       (map (fn [[a b]] b))))
+
 
 (deftest a-test
   (testing "delim-odd-pos"
-    (is (= '(2 4) (delim-odd-pos [1 2 3 4 5])))
+    (is (= [2 4] (delim-odd-pos [1 2 3 4 5])))
+    (is (= [2 4] (delim-odd-pos2 [1 2 3 4 5])))
     ))
 
--- hackerrank/src/hackerrank/list_replication.clj
+++ hackerrank/src/hackerrank/list_replication.clj
@@ -1,17 +1,28 @@
 (ns hackerrank.list-replication
   (:require [clojure.test :refer :all]))
 
-(defn list-replicate
-  ([num lst] (list-replicate num lst '()))
-  ([num lst res]
-   (if (empty? lst)
-     res
-     (recur num 
-            (rest lst)
-            (concat res 
-                    (take num (repeat (first lst))))))))
+(defn list-replicate [num lst]
+  (letfn [(iter [lst res]
+            (if (empty? lst)
+              res
+              (recur (rest lst)
+                     (concat res (take num (repeat (first lst)))))))]
+    (iter lst [])))
+
+
+(defn list-replicate2 [n coll]
+  (->> coll
+       (map #(take n (repeat %)))
+       (apply concat)))
+
+(defn list-replicate3 [n coll]
+  (->> (repeat coll)
+       (take n)
+       (apply interleave)))
 
 (deftest test-this
   (testing "test list-replicate"
     (is (= '(1 1 1 2 2 2 3 3 3) (list-replicate 3 [1 2 3])))
+    (is (= '(1 1 1 2 2 2 3 3 3) (list-replicate2 3 [1 2 3])))
+    (is (= '(1 1 1 2 2 2 3 3 3) (list-replicate3 3 [1 2 3])))
     ))
