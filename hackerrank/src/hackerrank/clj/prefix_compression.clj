09ce7bd7d9e89ea804ea4f3d235e5e4d5dc0d0ea
hifistar <yenshan@gk9.so-net.ne.jp>
2017-10-28 19:12:04 +0900

    add file, rewrite prefix_compression.clj

--- hackerrank/src/hackerrank/clj/prefix_compression.clj
+++ hackerrank/src/hackerrank/clj/prefix_compression.clj
@@ -1,16 +1,17 @@
 ;;
 ;; https://www.hackerrank.com/challenges/prefix-compression/problem
 ;;
-(ns hackerrank.clj.prefix-compression)
+(ns hackerrank.clj.prefix-compression
+  (:require [clojure.test :refer :all]))
 
-(defn get-prefix 
-  ([str1 str2] (get-prefix str1 str2 []))
-  ([str1 str2 res]
-   (if (or (empty? str1) (empty? str2)
-           (not= (first str1) (first str2)))
-     (map #(apply str %) (list res str1 str2))
-     (recur (rest str1) (rest str2) (conj res (first str1))))))
-     
+(defn get-prefix [str1 str2]
+  (loop [s1 str1, s2 str2, res []]
+    (if (or (empty? s1) (empty? s2)
+            (not= (first s1) (first s2)))
+      (map #(apply str %) (list res s1 s2))
+      (recur (rest s1) (rest s2) (conj res (first s1))))))
+
 (doseq [i (get-prefix (read-line) (read-line))]
   (println (count i) i))
+
 
