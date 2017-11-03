d8550353ef9e85f85988e807fb6a5fc89e9ca672
hifistar <yenshan@gk9.so-net.ne.jp>
2017-11-03 20:32:41 +0900

    revised implementation.

--- hackerrank/src/hackerrank/clj/convex_hull.clj
+++ hackerrank/src/hackerrank/clj/convex_hull.clj
@@ -1,20 +1,44 @@
 (ns hackerrank.clj.convex-hull
   (:require [clojure.string :as s]))
 
-
-(defn get-vector
-  [[x1 y1] coll]
-  (map (fn [[x2 y2]] [(- x2 x1) (- y2 y1)])
-       coll))
-
-(defn check-dir [vec-coll]
-  (loop [v vec-coll, res 0]
-    (if (empty? v)
+(defn get-most-left-pos [coll]
+  (loop [c (rest coll), res (first coll)]
+    (if (empty? c)
       res
-      (let [[x y] (first v)
-            x-bit (if (>= x 0) 2r0001 2r0010)
-            y-bit (if (>= y 0) 2r0100 2r1000)]
-        (recur (rest v) (bit-or res x-bit y-bit))))))
+      (recur (rest c)
+             (let [e (first c)
+                   [x1 _] res
+                   [x2 _] e]
+               (if (< x2 x1) e res))))))
+
+
+(defn slope [[x1 y1] [x2 y2]] 
+  (let [dx (- x2 x1)
+          dy (- y2 y1)]
+    (if (zero? dx)
+      (double (/ dy 0.00001))
+      (double (/ dy dx)))))
+
+(defn find-convex-pos [pos1 pos2 coll]
+  (let [rest-coll (remove #(or (= % pos1) (= % pos2)) coll)]
+    (->> (for [e rest-coll]
+                 {:pos e :slope (slope pos2 e)})
+         (sort #(compare (:slope %2) (:slope %1)))
+         first
+         :pos
+         )))
+
+(defn get-convex-points [coll]
+  (let [first-p (get-most-left-pos coll)]
+    (loop [p1 nil
+           p2 first-p
+           res [first-p]]
+      (let [next-p (find-convex-pos p1 p2 coll)]
+        (if (= next-p first-p)
+          res
+          (recur p2
+                 next-p
+                 (conj res next-p)))))))
 
 (defn perimeter
   [[x1 y1] [x2 y2]]
@@ -31,15 +55,10 @@
       (recur (rest d)
              (+ sum (perimeter (first d) (second d)))))))
 
-(defn convex_hull [pos-coll]
-  (->> (for [e pos-coll]
-         {:pos e :vecs (get-vector e (remove #(= % e) pos-coll))}
-         )
-       (filter #(not= 2r1111 (check-dir (:vecs %))))
-       (map :pos)
+(defn convex_hull [coll]
+  (->> (get-convex-points coll)
        vec
-       perimeter-around
-       ))
+       perimeter-around))
 
 (defn num->vec [nstr]
   (->> (s/split nstr #" ")
