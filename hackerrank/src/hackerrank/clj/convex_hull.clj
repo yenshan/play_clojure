6fe2cc0369baf3a270c8267fee4dbc2802ee15c0
hifistar <yenshan@gk9.so-net.ne.jp>
2017-11-04 12:27:17 +0900

    chaged to valid algorithm, and it works.

--- hackerrank/src/hackerrank/clj/convex_hull.clj
+++ hackerrank/src/hackerrank/clj/convex_hull.clj
@@ -1,3 +1,6 @@
+;;
+;; https://www.hackerrank.com/challenges/convex-hull-fp/problem
+;;
 (ns hackerrank.clj.convex-hull
   (:require [clojure.string :as s]))
 
@@ -11,44 +14,35 @@
                    [x2 _] e]
                (if (< x2 x1) e res))))))
 
+(defn get-vector [[x1 y1] [x2 y2]] [(- x2 x1) (- y2 y1)])
 
-(defn slope [[x1 y1] [x2 y2]] 
-  (let [dx (- x2 x1)
-          dy (- y2 y1)]
-    (if (zero? dx)
-      (double (/ dy 0.00001))
-      (double (/ dy dx)))))
+(defn cross-product
+  [[a b] [c d]]
+  (- (* a d) (* b c)))
+
+(defn left-convex-poit [pos coll]
+  (->> (remove #(= % pos) coll)
+       (map (fn [p] 
+              {:pos p :vec (get-vector pos p)})) 
+       (sort (fn [e1 e2] 
+               (if (> 0 (cross-product (:vec e1) (:vec e2))) true false)))
+       first
+       (:pos)
+       ))
+
+(defn get-convex-points [coll]
+  (let [first-p (get-most-left-pos coll)]
+    (loop [p first-p, res [first-p]]
+      (let [next-p (left-convex-poit p coll)]
+        (if (= next-p first-p)
+          res
+          (recur next-p (conj res next-p)))))))
+
 
 (defn perimeter
   [[x1 y1] [x2 y2]]
   (Math/sqrt (+ (Math/pow (- x2 x1) 2)
                 (Math/pow (- y2 y1) 2))))
-
-(defn find-convex-pos [pos1 pos2 coll]
-  (let [rest-coll (remove #(or (= % pos1) (= % pos2)) coll)]
-    (->> (for [e rest-coll]
-                 {:pos e :slope (slope pos2 e)})
-         ;(sort (fn [p1 p2] (compare (:slope p2) (:slope p1))))
-         (sort (fn [p1 p2] 
-                 (if (= (:slope p2) (:slope p1))
-                   (compare (perimeter pos2 (:pos p2))
-                            (perimeter pos2 (:pos p1)))
-                   (compare (:slope p2) (:slope p1)))))
-         first
-         :pos
-         )))
-
-(defn get-convex-points [coll]
-  (let [first-p (get-most-left-pos coll)]
-    (loop [p1 nil
-           p2 first-p
-           res [first-p]]
-      (let [next-p (find-convex-pos p1 p2 coll)]
-        (if (= next-p first-p)
-          res
-          (recur p2
-                 next-p
-                 (conj res next-p)))))))
 
 (defn perimeter-around 
   "coll should be vector"
