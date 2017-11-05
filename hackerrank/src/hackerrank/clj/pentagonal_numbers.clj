6234f78ee18abfc4c36320cd4d3f15bf5a1e442b
hifistar <yenshan@gk9.so-net.ne.jp>
2017-11-05 23:50:26 +0900

    revised implementation

--- hackerrank/src/hackerrank/clj/pentagonal_numbers.clj
+++ hackerrank/src/hackerrank/clj/pentagonal_numbers.clj
@@ -1,16 +1,18 @@
 (ns hackerrank.clj.pentagonal-numbers)
 
-(defn two-side-points [n]
-  (condp = n
-    1 1
+(defn two-side-points [^long n]
+  (if (= n 1)
+    1
     (+ 1 (* 2 (dec n)))))
 
-(defn P [n]
-  (condp = n
-    1 1
-    (- (+ (* 5 (dec n))
-          (P (dec n)))
-       (two-side-points (dec n)))))
+(defn P [^long N]
+  (loop [n N, res 0]
+    (if (= n 1)
+      (inc res)
+      (recur (dec n)
+             (-> res
+                 (+ (* 5 (dec n)))
+                 (- (two-side-points (dec n))))))))
 
 (doseq [_ (range (Integer/parseInt (read-line)))]
   (println (P (Integer/parseInt (read-line)))))
