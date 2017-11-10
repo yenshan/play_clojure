6e57aae6926d726dfb332ebbcc6404bf2abf4f34
hifistar <yenshan@gk9.so-net.ne.jp>
2017-11-10 09:44:04 +0900

    add file.

---
+++ hackerrank/src/hackerrank/algorithms/compare_the_triplets.clj
@@ -0,0 +1,19 @@
+(ns hackerrank.algorithms.compare-the-triplets
+  (:require [clojure.string :as s]))
+
+(defn str->nums [str]
+  (->> (s/split str #" ")
+       (map #(Integer/parseInt %))))
+
+(def a-score (str->nums (read-line)))
+(def b-score (str->nums (read-line)))
+
+(->> (map compare a-score b-score)
+     (reduce (fn [res cnd]
+               (cond (> cnd 0) (update res :alice inc)
+                     (< cnd 0) (update res :bob inc)
+                     :else res))
+             {:alice 0 :bob 0})
+     ((fn [{:keys [alice bob]}] (println alice bob))))
+
+            
