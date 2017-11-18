fcda9e7c707be590ef02a2447379a47abaff77c4
hifistar <yenshan@gk9.so-net.ne.jp>
2017-11-18 20:02:07 +0900

    refactored

--- hackerrank/src/hackerrank/algorithms/implementation/sock_merchant.clj
+++ hackerrank/src/hackerrank/algorithms/implementation/sock_merchant.clj
@@ -8,14 +8,20 @@
   (->> (s/split str #" ")
        (map #(Integer/parseInt %))))
 
+(defn count-sock-pairs [socks]
+  (loop [[[_ a] & rst] socks, res 0]
+    (if (empty? rst)
+      res
+      (let [pair (some (fn [[i e]] (when (= e a) i)) rst)]
+        (if pair
+          (recur (remove (fn [[i e]] (= i pair)) rst) (inc res))
+          (recur rst res))))))
+
 (def _ (read-line))
 
-(let [socks (map-indexed vector (str->nums (read-line)))]
-  (println 
-    (loop [[[_ a] & rst] socks, res 0]
-      (if (empty? rst)
-        res
-        (let [pair (some (fn [[i e]] (when (= e a) i)) rst)]
-          (if pair
-            (recur (remove (fn [[i e]] (= i pair)) rst) (inc res))
-            (recur rst res)))))))
+(->> (read-line)
+     str->nums
+     (map-indexed vector)
+     count-sock-pairs
+     println)
+
