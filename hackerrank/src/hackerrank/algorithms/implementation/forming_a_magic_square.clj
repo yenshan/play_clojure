45d91900b7b156e2769ff0bc78f68942ed47eca2
hifistar <yenshan@gk9.so-net.ne.jp>
2017-11-22 20:24:05 +0900

    completed implementation, but this solution is wrong.

--- hackerrank/src/hackerrank/algorithms/implementation/forming_a_magic_square.clj
+++ hackerrank/src/hackerrank/algorithms/implementation/forming_a_magic_square.clj
@@ -1,5 +1,13 @@
+;;
+;; https://www.hackerrank.com/challenges/magic-square-forming/problem
+;;
 (ns hackerrank.algorithms.implementation.forming-a-magic-square
   (:require [clojure.string :as s]))
+
+;;
+;; This solution could not pass all test cases.
+;; only passed Test Case #0, #21 #22
+;;
 
 (defn str->nums [str]
   (->> (s/split str #" ")
@@ -7,13 +15,6 @@
 
 (defn index->pos [i]
   [(mod i 3) (quot i 3)])
-
-(def dat (->> [[4 8 2] [4 5 7] [6 1 6]]
-              (reduce concat [])
-              (map-indexed vector)
-              (map (fn [[i n]] {:pos (index->pos i) :num n :flag true}))
-              vec
-              ))
 
 (defn row-is? [n]
   (fn [pos]
@@ -33,7 +34,9 @@
   [p]
   (= 2 (+ (first p) (second p))))
   
-(defn get-pos-sum-15 [f coll]
+(defn get-pos-sum-15
+  "collを方向関数fでチェックしてsumが15になるなら、この方向のデータ列の座標を返す"
+  [f coll]
   (let [dat (filter #(f (:pos %)) coll)
         sum (->> dat (map :num) (reduce +))]
   (if (= 15 sum)
@@ -43,7 +46,7 @@
 (defn is-in? [p coll]
   (some #(= p %) coll))
 
-
+;; 3 x 3の魔方陣をチェックするときの方向関数
 (def directions (concat (for [i (range 3)] (row-is? i))
                         (for [i (range 3)] (col-is? i))
                         (list diag-lt2rb?  diag-lb2rt?)))
@@ -59,7 +62,11 @@
          coll)))
 
 
-(defn not-sum-15? [f coll]
+(defn not-sum-15?
+  "collで方向関数fチェックしてsumが15でない場合に
+   {合計値、書き換え可能セル数、データ烈}のmapを返す。
+  それ以外はnilを返す"
+  [f coll]
   (let [dat (filter #(f (:pos %)) coll)
         sum (->> dat (map :num) (reduce +))
         wtbl (reduce (fn [res d] (if (:flag d) (inc res) res)) 0 dat)
@@ -69,50 +76,52 @@
       nil)))
 
 
-(defn get-modify-target [coll]
+(defn get-modify-info
+  "魔法陣の各方向でsumが15になるかチェックして、15にならない方向のデータ列のうち
+  書き換え可能なセルが1のもののみを抽出して、その座標と値とデータ烈のsumのmapを返す"
+  [coll]
   (let [dat1 (check-not-writable-pos coll)
         modify-target (->> (for [i directions]
                              (not-sum-15? i dat1))
-                           (filter (fn [d] (= 1 (:writable d)))))
+                           (filter (fn [d] (= 1 (:writable d))))
+                           set)
         ]
     (for [{sum :sum dat :dat}  modify-target]
       (let [mt (some (fn [d] (if (:flag d) d)) dat)]
-        {:sum sum :pos (:pos mt) :num (:num mt) }))))
+        {:new-dat {:pos (:pos mt) :num (+ (:num mt) (- 15 sum)) :flag true}
+         :dat mt}))))
 
-(get-modify-target dat)
+(defn modify-matrix-with-info [modify-info dat]
+  (map (fn [d]
+         (let [new-d (some (fn [dd] 
+                             (when (= (:dat dd) d)
+                               (:new-dat dd))) 
+                           modify-info)]
+           (if new-d new-d d)))
+       dat))
 
-;  (for [{sum :sum dat :dat}  modify-target]
-;    (let [mt (filter #(:flag %) dat)]
-;      {:sum sum :dat 
+(defn forming-magic-square
+  [square-dat]
+  (loop [dat square-dat, res '()]
+    (let [modify-info (set (get-modify-info dat))]
+      (if (empty? modify-info)
+        res
+        (recur (modify-matrix-with-info modify-info dat)
+               (concat res modify-info))))))
 
+(def dat (->> (for [_ (range 3)] (str->nums (read-line)))
+              (reduce concat [])
+              (map-indexed vector)
+              (map (fn [[i n]] {:pos (index->pos i) :num n :flag true}))
+              vec
+              ))
 
-(defn sum-when [f coll]
-  (->> coll
-       (filter #(f (:pos %)))
-       (map :num)
-       (reduce +)))
+(->> (forming-magic-square dat)
+     (map (fn [{:keys [new-dat dat]}]
+            (- (:num new-dat) (:num dat))))
+     (map #(Math/abs %))
+     (reduce +)
+     println
+     )
 
-(sum-when (row-is? 2) dat)
-(sum-when (col-is? 2) dat)
-(sum-when diag-lt2rb? dat)
-(sum-when diag-lb2rt? dat)
-
-;
-;(defn sum-diag-1 [coll]
-;  (reduce + (for [i (range 3)]
-;              (first (get coll (index i i))))))
-;
-;(defn sum-diag-2 [coll]
-;  (reduce + (for [i (range 3)]
-;              (first (get coll (index (- 2 i) i))))))
-;
-;(concat 
-;  (for [i (range 3)]
-;    (sum-row i dat))
-;  (for [i (range 3)]
-;    (sum-col i dat))
-;  [(sum-diag-1 dat) (sum-diag-2 dat)])
-
-
-  
 
