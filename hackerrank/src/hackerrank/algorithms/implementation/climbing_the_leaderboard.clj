b6be00c9ac176e4701e1367e7b7ed66c44229426
hifistar <yenshan@gk9.so-net.ne.jp>
2017-11-25 22:43:57 +0900

    passed all test cases.

--- hackerrank/src/hackerrank/algorithms/implementation/climbing_the_leaderboard.clj
+++ hackerrank/src/hackerrank/algorithms/implementation/climbing_the_leaderboard.clj
@@ -1,36 +1,40 @@
+;;
+;; https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem
+;;
 (ns hackerrank.algorithms.implementation.climbing-the-leaderboard
   (:require [clojure.string :as s]))
+
+;;
+;; GOOD problem: 
+;; This prolem set time limitation, it let me think efficient algorithm. 
+;; Using map is the most effective solution to pass all test cases in time limitation.
+;;
 
 (defn str->nums [str]
   (->> (s/split str #" ")
        (map #(Integer/parseInt %))))
 
-
-(defn create-rank-list [coll]
-  (sort #(compare (:rank %1) (:rank %2))
-        (vec
-          (loop [[sc & rst] coll, rank 1, rank-list #{}]
-            (if (empty? rst)
-              (conj rank-list {:rank rank :score sc})
-              (recur rst
-                     (if (> sc (first rst)) (inc rank) rank)
-                     (conj rank-list {:rank rank :score sc})))))))
-
 (defn check-rank [n coll]
-  (let [rank (:rank (some (fn [d]
-                            (when (<= (:score d) n) d))
-                          coll))]
-    (if rank
-      rank
-      (inc (:rank (last coll))))))
+  (let [sz (count coll)]
+    (loop [l 0, r (dec sz)]
+      (let [p (+ l (quot (- r l) 2))]
+        (cond 
+          (= l r) (inc l)
+          (> n (get coll l)) (inc l)
+          (< n (get coll r)) (+ r 2)
+          (< n (get coll p)) (recur (inc p) r)
+          (> n (get coll p)) (recur l p)
+          :else (inc p))))))
 
 (defn rank-history [your-scores rank-scores]
-  (let [rklst (create-rank-list rank-scores)]
-    (loop [[a & rst] your-scores, res []]
-      (if (nil? a)
-        res
-        (recur rst
-               (conj res (check-rank a rklst)))))))
+  (let [rkmap (->> (vec (set rank-scores))
+                   (sort #(compare %2 %1))
+                   (map-indexed vector)
+                   (reduce (fn [res [i d]]
+                             (assoc res i d))
+                           {}))]
+    (for [sc your-scores]
+      (check-rank sc rkmap))))
 
 (let [_ (read-line)
       rank-scores (str->nums (read-line))
