3db81fec39a3080e9ec00dfe8616924a3286fca3
hifistar <yenshan@gk9.so-net.ne.jp>
2017-12-16 22:31:46 +0900

    performance improved three times faster than previous version, but still could not pass all tests.

--- hackerrank/src/hackerrank/algorithms/implementation/bigger_is_greater.clj
+++ hackerrank/src/hackerrank/algorithms/implementation/bigger_is_greater.clj
@@ -4,46 +4,51 @@
   {(first b) (second a)
    (first a) (second b)})
 
+(defn drop-nth [n coll]
+  (->> (map-indexed vector coll)
+       (remove (fn [[i _]] (= i n )))
+       (map second)))
+
 (defn next-bigger-string [string]
   (let [st-col (map-indexed vector string)
-        st-array (to-array st-col)
-        len (count st-array)
+        st-map (reduce (fn [res [i c]]
+                         (assoc res i c))
+                       {}
+                       st-col)
+        len (count string)
         exc-pair (loop [i (dec len)]
-               (if (< i 0)
-                 nil
-                 (let [sd (get st-array i)
-                       t (for [ti (range (inc i) len)
-                               :let [td (get st-array ti)]
-                               :when (< (compare (second sd) (second td)) 0)]
-                           td)]
-                   (if (empty? t)
-                     (recur (dec i))
-                     (let [std (sort-by second t)]
-                       [sd (first std)])))))
+                   (if (< i 0)
+                     nil
+                     (let [sd (st-map i)
+                           t (loop [ti (inc i), res nil]
+                               (if (= ti len)
+                                 res
+                                 (let [td (st-map ti)]
+                                   (if (< (compare sd td) 0)
+                                     (cond (nil? res) (recur (inc ti) [ti td])
+                                           (< (compare td (second res)) 0) (recur (inc ti) [ti td])
+                                           :else (recur (inc ti) res))
+                                     (recur (inc ti) res)))))
+                           ]
+                       (if (nil? t)
+                         (recur (dec i))
+                         [[i sd] t]))))
         ]
     (when exc-pair
-      (let [rep-tgt-i (first (first exc-pair))
-            exc-info (make-exch-info (first exc-pair) (second exc-pair))
-            n-stcol (map (fn [d]
-                           (let [ei (exc-info (first d))]
-                             (if ei
-                               [(first d) ei]
-                               d)))
-                         st-col)
-            front-chars (filter #(<= (first %) rep-tgt-i) n-stcol)
-            rear-chars (filter #(> (first %) rep-tgt-i) n-stcol)
-            rear-sorted (sort-by second rear-chars)
-            answer (->> (concat front-chars rear-sorted)
-                        (map second) 
-                        (apply str))
-
+      (let [[[si sd] [ti td]] exc-pair
+            front-coll (concat (take si string) (list td))
+            rear-coll (sort (cons sd (drop (inc si) (drop-nth ti string))))
+            answer (apply str (concat front-coll rear-coll))
             ]
-        answer))))
+        answer
+      ))
+    ))
 
 (let [n (Integer/parseInt (read-line))]
   (doseq [_ (range n)]
     (let [string (read-line)
-          answer (next-bigger-string string)]
+          answer (next-bigger-string string)
+          ]
       (if (nil? answer)
         (println "no answer")
         (println answer)))))
