b3cd3087de8a140db1f9dc1cbd132f1c112fa853
hifistar <yenshan@gk9.so-net.ne.jp>
2017-12-16 15:27:34 +0900

    faster alogorithm, but not fast enough to pass all tests.

--- hackerrank/src/hackerrank/algorithms/implementation/bigger_is_greater.clj
+++ hackerrank/src/hackerrank/algorithms/implementation/bigger_is_greater.clj
@@ -1,41 +1,50 @@
-(ns hackerrank.algorithms.implementation.bigger-is-greater
-  (:require [clojure.string :as s]))
+(ns hackerrank.algorithms.implementation.bigger-is-greater)
 
-(defn str->nums [str]
-  (->> (s/split str #" ")
-       (map #(Integer/parseInt %))))
+(defn make-exch-info [a b]
+  {(first b) (second a)
+   (first a) (second b)})
 
-(defn drop-nth-char [n string]
-  (->> (map-indexed vector string)
-       (filter (fn [[i _]] (not= i n)))
-       (map second)
-       (apply str)
-       ))
+(defn next-bigger-string [string]
+  (let [st-col (map-indexed vector string)
+        st-array (to-array st-col)
+        len (count st-array)
+        exc-pair (loop [i (dec len)]
+               (if (< i 0)
+                 nil
+                 (let [sd (get st-array i)
+                       t (for [ti (range (inc i) len)
+                               :let [td (get st-array ti)]
+                               :when (< (compare (second sd) (second td)) 0)]
+                           td)]
+                   (if (empty? t)
+                     (recur (dec i))
+                     (let [std (sort-by second t)]
+                       [sd (first std)])))))
+        ]
+    (when exc-pair
+      (let [rep-tgt-i (first (first exc-pair))
+            exc-info (make-exch-info (first exc-pair) (second exc-pair))
+            n-stcol (map (fn [d]
+                           (let [ei (exc-info (first d))]
+                             (if ei
+                               [(first d) ei]
+                               d)))
+                         st-col)
+            front-chars (filter #(<= (first %) rep-tgt-i) n-stcol)
+            rear-chars (filter #(> (first %) rep-tgt-i) n-stcol)
+            rear-sorted (sort-by second rear-chars)
+            answer (->> (concat front-chars rear-sorted)
+                        (map second) 
+                        (apply str))
 
-(defn all-str-pattern [string]
-  (letfn [(iter [st res]
-            (if (empty? st)
-              res
-              (let [rt (for [i (range (count st))]
-                         (iter (drop-nth-char i st)
-                               (str res (nth st i))))
-                    ]
-                (if (= 1 (count rt))
-                  rt
-                  (apply concat rt)))))]
-    (iter string "")))
+            ]
+        answer))))
 
 (let [n (Integer/parseInt (read-line))]
   (doseq [_ (range n)]
     (let [string (read-line)
-          answer (->> (all-str-pattern string)
-                      (filter #(< 0 (compare % string)))
-                      sort
-                      first)]
+          answer (next-bigger-string string)]
       (if (nil? answer)
         (println "no answer")
         (println answer)))))
-
-
-  
 
