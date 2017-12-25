8521226554aead550d76b7f4ec0bb41b96514bc0
hifistar <yenshan@gk9.so-net.ne.jp>
2017-12-25 23:53:22 +0900

    revised to more concise implementation

--- hackerrank/src/hackerrank/algorithms/implementation/lisas_workbook.clj
+++ hackerrank/src/hackerrank/algorithms/implementation/lisas_workbook.clj
@@ -15,15 +15,13 @@
       (recur (drop k d)
              (conj res (set (take k d)))))))
 
-(let [[n k] (str->nums (read-line))
-      dat (str->nums (read-line))
-      problems (map (fn [nn]
-                      (take nn (iterate inc 1)))
-                    dat)
-      pages (apply concat (map #(-partition k %) problems))
-      page-indexed (map-indexed #(vector (inc %1) %2) pages)
-      specials (filter (fn [[i d]] (contains? d i)) page-indexed)
-      ]
-  (println (count specials))
-  )
+(let [[_ k] (str->nums (read-line))]
+  (->> (str->nums (read-line))
+       (map #(range 1 (inc %)))
+       (map #(-partition k %))
+       (apply concat)
+       (map-indexed #(vector (inc %1) %2))
+       (filter (fn [[i d]] (contains? d i)))
+       count
+       println))
 
