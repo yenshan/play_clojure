f48be6aa7c6de3368f10535e12cd19114898762b
hifistar <yenshan@gk9.so-net.ne.jp>
2017-12-30 13:15:05 +0900

    passed in timelimit. good problem.

--- hackerrank/src/hackerrank/algorithms/implementation/manasa_and_stones.clj
+++ hackerrank/src/hackerrank/algorithms/implementation/manasa_and_stones.clj
@@ -1,25 +1,28 @@
+;;
+;; https://www.hackerrank.com/challenges/manasa-and-stones/problem
+;;
 (ns hackerrank.algorithms.implementation.manasa-and-stones
   (:require [clojure.string :as s]))
 
+;;
+;; not difficult good problem.
+;; You need changing point of view to solve this in O(n).
+;;
+
 (defn last-nums [n a b]
-  (letfn [(iter [cnt res]
-            (if (zero? cnt)
-              (reduce + res)
-              (let [ret (for [x [a b]]
-                          (iter (dec cnt)
-                                (conj res x)))]
-                (if (<= cnt 1)
-                  ret
-                  (apply concat ret)))))
-          ]
-    (sort (set (iter n [])))))
+  (->> (for [i (range n)]
+         [i (- n 1 i)])
+       (map (fn [[na nb]]
+              (+ (* na a) (* nb b))))
+       set
+       sort))
 
 (let [T (Integer/parseInt (read-line))]
   (doseq [_ (range T)]
     (let [n (Integer/parseInt (read-line))
           a (Integer/parseInt (read-line))
           b (Integer/parseInt (read-line))]
-      (loop [[a & rst] (last-nums (dec n) a b)]
+      (loop [[a & rst] (last-nums n a b)]
         (when a
           (do (print a)
               (when (not (empty? rst))
