83c86a52802610e5f3e4c4b356ead27078046360
hifistar <yenshan@gk9.so-net.ne.jp>
2018-01-01 15:19:53 +0900

    Solved it. Good problem.

--- hackerrank/src/hackerrank/algorithms/implementation/the_grid_search.clj
+++ hackerrank/src/hackerrank/algorithms/implementation/the_grid_search.clj
@@ -1,5 +1,14 @@
+;;
+;; https://www.hackerrank.com/challenges/the-grid-search/problem
+;;
 (ns hackerrank.algorithms.implementation.the-grid-search
   (:require [clojure.string :as s]))
+
+;;
+;; Good problem!!
+;; You need consider efficient data process in Clojure to solved this problem in the limited time. (maybe in 3 seconds for test05)
+;; Using "clojure.string/index-of" is the best way.
+;;
 
 (defn str->nums [str]
   (->> (s/split str #" ")
@@ -12,33 +21,46 @@
       (recur (dec n) 
              (conj res (read-line))))))
 
+(defn all-index-of [col ptn]
+  (let [len-col (count col)
+        len-ptn (count ptn)]
+    (loop [i 0, res []]
+      (if (< len-col (+ i len-ptn))
+        res
+        (if-let [fi (s/index-of col ptn i)]
+          (recur (+ fi 1) (conj res fi))
+          res)))))
+
 (let [T (Integer/parseInt (read-line))]
   (doseq [i (range T)]
     (let [[R C] (str->nums (read-line))
-          matrix (->> (read-matrix R)
-                      (apply str)
-                      to-array
-                      )
-          matrix-len (count matrix)
+          matrix (to-array (read-matrix R))
           [r c] (str->nums (read-line))
-          index (fn [r c] (+ c (* r C)))
-          pattern (to-array (apply concat (read-matrix r)))
-          ptn-len (count pattern)
-          ptn-idx (to-array (for [ir (range r) ic (range c)] (index ir ic)))
-          first-c (get pattern 0)
-          ptn-is-in (loop [i 0]
-                      (if (<= matrix-len (+ i ptn-len))
-                        nil
-                        (if (or (< C (+ (mod i C) c))
-                                (not= first-c (get matrix i)))
-                          (recur (inc i))
-                          (if (every? #(= (get matrix (+ i (get ptn-idx %)))
-                                          (get pattern %))
-                                      (range ptn-len))
-                              true
-                              (recur (inc i))))))
+          pattern (to-array (read-matrix r))
+          
+          first-ptn (get pattern 0)
+          first-ptn-idxs (apply concat 
+                                (for [mrow (range R)
+                                      :let [line (get matrix mrow)
+                                            idxs (all-index-of line first-ptn)
+                                            ]
+                                      :when (not (empty? idxs))
+                                      ]
+                                  (map (fn [d] [mrow d]) idxs)
+                                  ))
+          rest-ptn-res (for [[fr fc] first-ptn-idxs]
+                         (for [pi (range 1 r)
+                               :let [ptn (get pattern pi)
+                                     line (get matrix (+ fr pi))
+                                     ptn-c (s/index-of line ptn fc)
+                                     ]
+                               ]
+                           (if (= fc ptn-c)
+                             true
+                             nil)))
+          result (map #(every? true? %) rest-ptn-res)
           ]
-      (if ptn-is-in
+      (if (some true? result)
         (println "YES")
         (println "NO"))
      )))
