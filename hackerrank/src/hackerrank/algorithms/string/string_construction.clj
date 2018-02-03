5c753cb7a18b61171b735224788baf53d6475b96
hifistar <yenshan@gk9.so-net.ne.jp>
2018-02-03 12:52:44 +0900

    easy. use reduce for more clean implementation.

--- hackerrank/src/hackerrank/algorithms/string/string_construction.clj
+++ hackerrank/src/hackerrank/algorithms/string/string_construction.clj
@@ -1,20 +1,16 @@
 ;;
 ;; :https://www.hackerrank.com/challenges/string-construction/problem
 ;;
-(ns hackerrank.algorithms.string.string-construction
-  (:require [clojure.string :as s]))
+(ns hackerrank.algorithms.string.string-construction)
 
 (let [n (Integer/parseInt (read-line))]
   (doseq [_ (range n)]
-    (let [string (read-line)
-          cnt (loop [[a & rst] string, char-map {}, cnt 0]
-                (if (nil? a)
-                  cnt
-                  (if (get char-map a)
-                    (recur rst char-map cnt)
-                    (recur rst
-                           (assoc char-map a 1)
-                           (inc cnt)))))
-          ]
-      (println cnt))))
+    (->> (read-line)
+         (reduce (fn [[cnt char-map] c]
+                   (if (get char-map c)
+                     [cnt char-map]
+                     [(inc cnt) (assoc char-map c 1)]))
+                 [0 {}])
+         first
+         println)))
 
