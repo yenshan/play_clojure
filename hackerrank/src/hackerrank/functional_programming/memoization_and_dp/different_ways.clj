6c6436ed7727116bd00bafde79129e653f0e48c4
hifistar <yenshan@gk9.so-net.ne.jp>
2018-02-11 08:36:37 +0900

    easy, fun. This is basic of basics.

--- hackerrank/src/hackerrank/functional_programming/memoization_and_dp/different_ways.clj
+++ hackerrank/src/hackerrank/functional_programming/memoization_and_dp/different_ways.clj
@@ -1,3 +1,6 @@
+;;
+;; https://www.hackerrank.com/challenges/different-ways-fp/problem
+;;
 (ns hackerrank.functional-programming.memoization-and-dp.different-ways
   (:require [clojure.string :as s]))
 
@@ -5,14 +8,22 @@
   (->> (s/split str #" ")
        (map #(Integer/parseInt %))))
 
+(def mod-base (+ 7 (apply * (take 8 (repeat 10)))))
+
+(defn factorial [number]
+  (loop [n number, res 1N]
+    (if (zero? n)
+      res
+      (recur (dec n) (* n res)))))
+
 (defn count-lemur [n k]
-  (cond (zero? k) 1
-        (= k n) 1
-        :else (+ (count-lemur (dec n) (dec k))
-                 (count-lemur (dec n) k))))
+  (/ (factorial n)
+     (* (factorial k)
+        (factorial (- n k)))))
 
 (let [T (Integer/parseInt (read-line))]
   (doseq [_ (range T)]
     (let [[N K] (str->nums (read-line))]
-    (println (count-lemur N K)))))
+    (println (str (mod (count-lemur N K)
+                       mod-base))))))
 
