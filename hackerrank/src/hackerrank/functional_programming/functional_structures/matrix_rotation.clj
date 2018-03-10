7fa28162b7d8ea238d03686ae446991457e7dc51
hifistar <yenshan@gk9.so-net.ne.jp>
2018-03-10 16:54:06 +0900

    solved. good practice to think efficient algorithm.

--- hackerrank/src/hackerrank/functional_programming/functional_structures/matrix_rotation.clj
+++ hackerrank/src/hackerrank/functional_programming/functional_structures/matrix_rotation.clj
@@ -1,3 +1,6 @@
+;;
+;; https://www.hackerrank.com/challenges/matrix-rotation/problem
+;;
 (ns hackerrank.functional-programming.functional-structures.matrix-rotation
   (:require [clojure.string :as s]))
 
@@ -5,25 +8,25 @@
   (->> (s/split str #" ")
        (map #(Integer/parseInt %))))
 
-(defn edge [f k o mtxm]
+(defn side [f k o mtxm]
   (drop-last 
     (let [elem (filter f mtxm)]
       (condp = o
         :inorder (sort-by k elem)
         :reverse (sort #(compare (get %2 k) (get %1 k)) elem)))))
 
-(defn take-edge [rmin rmax cmin cmax mtxm]
+(defn take-four-sides [rmin rmax cmin cmax mtxm]
   (let [top-e #(and (= rmin (:r %)) (<= cmin (:c %) cmax))
         right-e #(and (= cmax (:c %)) (<= rmin (:r %) rmax))
         bottom-e #(and (= rmax (:r %)) (<= cmin (:c %) cmax))
         left-e #(and (= cmin (:c %)) (<= rmin (:r %) rmax))
         ]
-  (concat (edge top-e :c :inorder mtxm) 
-          (edge right-e :r :inorder mtxm)
-          (edge bottom-e :c :reverse mtxm)
-          (edge left-e :r :reverse mtxm))))
+  (concat (side top-e :c :inorder mtxm) 
+          (side right-e :r :inorder mtxm)
+          (side bottom-e :c :reverse mtxm)
+          (side left-e :r :reverse mtxm))))
 
-(defn matrix->edgelist [rows cols mtxm]
+(defn matrix->sideslist [rows cols mtxm]
   (loop [rmin 0
          rmax (dec rows)
          cmin 0
@@ -35,7 +38,7 @@
              (dec rmax)
              (inc cmin)
              (dec cmax)
-             (conj res (take-edge rmin rmax cmin cmax mtxm))))))
+             (conj res (take-four-sides rmin rmax cmin cmax mtxm))))))
 
 (defn rotate-seq-left [n coll]
   (let [rn (mod n (count coll))]
@@ -47,12 +50,20 @@
   (map #(assoc %1 :d %2) mtxm coll))
 
 (defn rotate-matrix-left [n rows cols mtxm]
-  (->> mtxm
-       (matrix->edgelist rows cols)
-       (map #(->> (map :d %)
-                  (rotate-seq-left n)
-                  (update-edge %)))
-       (apply concat)))
+  (let [sidelist (matrix->sideslist rows cols mtxm)
+        rot-res (map #(rotate-seq-left n (map :d %)) sidelist)
+        ]
+    (loop [[x & xs] sidelist
+           [y & ys] rot-res
+           res {}]
+      (if (nil? x)
+        res
+        (recur xs
+               ys
+               (reduce (fn [r [a b]]
+                         (assoc r [(:r a) (:c a)] b))
+                       res
+                       (map vector x y)))))))
 
 (defn print-matrix [rows cols mtxm]
   (doseq [r (range rows)
@@ -69,10 +80,7 @@
                       (map (fn [[i d]]
                              {:r row :c i :d d })
                            (map-indexed vector (str->nums (read-line))))))
-      res (->> (rotate-matrix-left R M N mtx-map)
-               (reduce (fn [res d]
-                         (assoc res [(:r d) (:c d)] (:d d)))
-                       {}))
+      res (rotate-matrix-left R M N mtx-map)
       ]
   (print-matrix M N res))
 
