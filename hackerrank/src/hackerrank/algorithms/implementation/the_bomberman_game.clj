28840049084ef5d70bb4c5b960dab295b924280e
hifistar <yenshan@gk9.so-net.ne.jp>
2018-03-11 23:17:56 +0900

    changed algorithm and improved performance, but still could not pass some tests due to time out.

--- hackerrank/src/hackerrank/algorithms/implementation/the_bomberman_game.clj
+++ hackerrank/src/hackerrank/algorithms/implementation/the_bomberman_game.clj
@@ -5,75 +5,61 @@
   (->> (s/split str #" ")
        (map #(Integer/parseInt %))))
 
-(defn print-matrix [mtxm rows cols]
-  (doseq [r (range rows), c (range cols)]
-    (print (if (= nil (get mtxm [r c]))
-             \. \O))
-    (when (= c (dec cols)) (println ""))))
-
-
-(defn plant-bombs [mtxm bmb]
-  (reduce (fn [res d]
-            (if (= (get res d) nil)
-              (assoc res d bmb)
-              res))
-          mtxm
-          (keys mtxm)))
 
 (defn up [[r c]] [(dec r) c])
 (defn down [[r c]] [(inc r) c])
 (defn left [[r c]] [r (dec c)])
 (defn right [[r c]] [r (inc c)])
 
-(defn detonate [mtxm pos]
-  (reduce (fn [res d]
-            (if (or (= d pos)
-                    (= d (up pos))
-                    (= d (down pos))
-                    (= d (left pos))
-                    (= d (right pos))) (assoc res d nil)
-              res))
-          mtxm
-          (keys mtxm)))
 
-(defn detonate-all [mtxm bmb]
-  (let [bmb-pos (filter (fn [[_ b]] (= bmb b)) mtxm)]
-    (reduce (fn [res d]
-              (detonate res d))
-            mtxm
-            (keys bmb-pos))))
+(defn plant-bombs [rows cols bombs]
+  (loop [[p & rst] (for [r (range rows) c (range cols)] [r c])
+         res {}]
+    (if (nil? p)
+      res
+      (recur rst
+             (if (get bombs p) res (assoc res p :bmb))))))
 
-(defn next-bmb-type [bt]
-  (if (= bt :bmb1) :bmb1 :bmb2))
+(defn detonate [bombs]
+  (loop [[[p d :as b] & rst] (vec bombs)
+         res {}]
+    (if (nil? b)
+      res
+      (recur rst (-> res
+                     (assoc p :bmb)
+                     (assoc (up p) :bmb)
+                     (assoc (down p) :bmb)
+                     (assoc (left p) :bmb)
+                     (assoc (right p) :bmb))))))
 
-(defn plant-detonate-loop [mtxm n]
-  (loop [cnt 1
-         res mtxm
-         plant-bmb :bmb2
-         deto-bmb :bmb1
-         ]
-    (cond (> cnt n) res
-          (odd? cnt) (recur (inc cnt) 
-                            (plant-bombs res plant-bmb)
-                            (next-bmb-type plant-bmb)
-                            deto-bmb)
-          :else (recur (inc cnt)
-                       (detonate-all res deto-bmb)
-                       plant-bmb
-                       (next-bmb-type deto-bmb)))))
+(defn print-matrix [bmbm rows cols]
+  (doseq [r (range rows), c (range cols)]
+    (print (if (get bmbm [r c]) \O  \.))
+    (when (= c (dec cols)) (println ""))))
 
+(defn print-all-bombs [rows cols]
+  (doseq [r (range rows), c (range cols)]
+    (print \O)
+    (when (= c (dec cols)) (println ""))))
 
 (let [[R C N] (str->nums (read-line))
-      matrix (loop [[r & rst] (range R), res {}]
+      bombs (loop [[r & rst] (range R), res {}]
                (if (nil? r)
                  res
                  (let [line (map-indexed vector (read-line))]
                    (recur rst
                           (reduce (fn [rs [i c]]
-                                    (assoc rs [r i] (if (= c \O) :bmb1 nil)))
+                                    (if (= c \O)
+                                      (assoc rs [r i] :bmb)
+                                      rs))
                                   res
                                   line)))))
-      res (plant-detonate-loop matrix (dec N))
       ]
-  (print-matrix res R C))
+  (if (even? N)
+    (print-all-bombs R C)
+    (let [res (reduce (fn [m _]
+                        (plant-bombs R C (detonate m)))
+                      bombs
+                      (range 3 (inc N) 2))]
+      (print-matrix res R C))))
 
