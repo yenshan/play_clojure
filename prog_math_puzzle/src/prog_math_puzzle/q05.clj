1ca5da27b58ac9936d57aea338150f672763f79a
hifistar <yenshan@gk9.so-net.ne.jp>
2018-03-22 23:37:37 +0900

    change to function

--- prog_math_puzzle/src/prog_math_puzzle/q05.clj
+++ prog_math_puzzle/src/prog_math_puzzle/q05.clj
@@ -1,16 +1,18 @@
 (ns prog-math-puzzle.q05)
 
 
-(reduce +
-        (for [m10 (range 0 (inc 15))
-              m50 (range 0 (inc 15))
-              m100 (range 0 (inc 15))
-              m500 (range 0 (inc 15))
-              :when (and (<= (+ m10 m50 m100 m500) 15)
-                         (= 1000 (+ (* 10 m10)
-                                    (* 50 m50)
-                                    (* 100 m100)
-                                    (* 500 m500))))
-              ]
-          1))
+(defn change_coin [money max_coins]
+  (reduce +
+          (for [m10 (range 0 (inc max_coins))
+                m50 (range 0 (inc max_coins))
+                m100 (range 0 (inc max_coins))
+                m500 (range 0 (inc max_coins))
+                :when (and (<= (+ m10 m50 m100 m500) max_coins)
+                           (= money (+ (* 10 m10)
+                                       (* 50 m50)
+                                       (* 100 m100)
+                                       (* 500 m500))))
+                ]
+            1)))
 
+(println (change_coin 1000 15))
