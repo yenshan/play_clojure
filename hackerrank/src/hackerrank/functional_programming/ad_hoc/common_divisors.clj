6f636318b13d620f145e09d51102a366afb6c494
hifistar <yenshan@gk9.so-net.ne.jp>
2018-04-15 15:14:16 +0900

    good problem, you should optimize performance.

---
+++ hackerrank/src/hackerrank/functional_programming/ad_hoc/common_divisors.clj
@@ -0,0 +1,36 @@
+;;
+;; https://www.hackerrank.com/challenges/common-divisors/problem
+;;
+(ns hackerrank.functional-programming.ad-hoc.common-divisors
+  (:require [clojure.string :as s]))
+
+(defn str->nums [str]
+  (->> (s/split str #" ")
+       (map #(Integer/parseInt %))))
+
+(defn gcd [a b]
+  (if (zero? b)
+    a
+    (recur b (mod a b))))
+
+(defn count-if [maxn f b]
+  (loop [n 1, cnt 0]
+    (if (> n (quot maxn b))
+      cnt
+      (recur (f n)
+             (+ cnt (if (zero? (mod maxn n)) 1 0))))))
+
+(defn count-common-divisors [n1 n2]
+  (let [ngcd (gcd (max n1 n2) (min n1 n2))]
+    (+ 1
+       (if (odd? ngcd)
+         (count-if ngcd #(+ % 2) 3)
+         (count-if ngcd inc 2)))))
+
+(defn -run []
+  (let [T (Integer/parseInt (read-line))]
+    (doseq [_ (range T)]
+      (let [[M R] (str->nums (read-line))]
+        (println (count-common-divisors M R))))))
+
+(-run)
