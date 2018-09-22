5f3d1268a27dd8e27c6c99b8b9e4db837b58b5b3
hifistar <yenshan@gk9.so-net.ne.jp>
2018-09-22 21:39:28 +0900

    easy, but failed passed some times. you need bignum.

--- hackerrank/src/hackerrank/algorithms/greedy/marks_cakewalk.clj
+++ hackerrank/src/hackerrank/algorithms/greedy/marks_cakewalk.clj
@@ -5,33 +5,22 @@
   (->> (s/split str #" ")
        (map #(Integer/parseInt %))))
 
-(defn remove-i [i coll]
-  (loop [[x & xs] coll
-         n 0
-         res []]
-  (cond (nil? x) res
-        (= n i) (recur xs (inc n) res)
-        :else (recur xs (inc n) (conj res x)))))
-
-(defn cal-ptns [coll]
-  (if (empty? coll)
-    [[]]
-    (apply concat 
-           (for [i (range (count coll))]
-             (map #(cons (get coll i) %)
-                  (cal-ptns (remove-i i coll)))))))
+(defn exp2 [n]
+  (if (zero? n)
+    1
+    (reduce #(* %1 %2) 1 (repeat n 2))))
 
 (defn sum-cals [coll]
-  (int (reduce (fn [s [i d]]
-                 (+ s (* (Math/pow 2 i)
-                         d)))
-               0
-               (map-indexed vector coll))))
+  (reduce (fn [s [i d]]
+            (+ s (* (exp2 i)
+                    d)))
+          0N
+          (map-indexed vector coll)))
 
 (let [_ (read-line)
-      cals (vec (str->nums (read-line)))
+      cals (str->nums (read-line))
      ] 
-  (->> (cal-ptns cals)
-       (map #(sum-cals %))
-       (apply min)
+  (->> (sort #(compare %2 %1) cals)
+       sum-cals
+       str
        println))
