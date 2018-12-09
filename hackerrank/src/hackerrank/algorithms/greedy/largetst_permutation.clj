229ab12d40a8207289262d3f61976efd310dee45
hifistar <yenshan@gk9.so-net.ne.jp>
2018-12-09 09:03:41 +0900

    change algorithms but still time cosuming.

--- hackerrank/src/hackerrank/algorithms/greedy/largetst_permutation.clj
+++ hackerrank/src/hackerrank/algorithms/greedy/largetst_permutation.clj
@@ -5,32 +5,40 @@
   (->> (s/split str #" ")
        (map #(Integer/parseInt %))))
 
-(defn swap [i j nums]
-  (assoc nums i (get nums j) j (get nums i)))
 
-(defn max-i [di nums]
-  (loop [i (- (count nums) 2)
-         ss (- (count nums) 1)]
-    (if (< i di)
-      ss
-      (let [in (get nums i)
-            ssn (get nums ss)]
-        (if (> in ssn) 
-          (recur (dec i) i)
-          (recur (dec i) ss))))))
+(defn maxv [m-i-val from]
+  (loop [i (inc from)
+         mx (get m-i-val from)]
+    (if (>= i (count m-i-val))
+      mx
+      (let [d (get m-i-val i)]
+        (if (> d mx)
+          (recur (inc i) d)
+          (recur (inc i) mx))))))
 
-(defn swap-loop [nums n]
-  (loop [si 0
-         cnt 0
-         res nums]
-      (if (or (>= si (- (count nums) 1)) 
-              (= cnt n))
-        res
-        (let [maxi (max-i si res)
-              ]
-          (if (= maxi si)
-            (recur (inc si) cnt res)
-            (recur (inc si) (inc cnt) (swap si maxi res)))))))
+(defn swap-n [i n m-val-i m-i-val]
+  (if (<= n 0)
+    m-val-i
+    (let [maxval (maxv m-i-val i)
+          maxvi (get m-val-i maxval)
+          ival (get m-i-val i)]
+      (recur (inc i)
+             (dec n)
+             (-> m-val-i (assoc maxval i) (assoc ival maxvi))
+             (-> m-i-val (assoc i maxval) (assoc maxvi ival))))))
+
+(defn do-swap-loop [nums k]
+  (let [nums-i (map-indexed vector nums)
+        m-i-val (reduce (fn [m [i d]]
+                          (assoc m i d))
+                        {}
+                        nums-i)
+        m-val-i (reduce (fn [m [i d]]
+                          (assoc m d i))
+                        {}
+                        nums-i)]
+    (map first (sort-by second (vec (swap-n 0 k m-val-i m-i-val))))))
+
 
 (defn print-nums [[x & xs]]
   (if (nil? x)
@@ -38,9 +46,7 @@
     (do (print x "") (recur xs))))
 
 (let [[l k] (str->nums (read-line))
-      nums (vec (str->nums (read-line)))
-      ]
-  (if (> k l)
+      nums (str->nums (read-line))]
+  (if (< l k)
     (print-nums (sort #(compare %2 %1) nums))
-    (->> (swap-loop nums k)
-         print-nums)))
+    (print-nums (do-swap-loop nums k))))
